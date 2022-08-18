87fa0ec818a9cd3adacbb43e8b6833392c43e30c
jpwalls08 <jpwalls08@outlook.com>
2022-08-17 20:03:59 -0500

    unfinsihed edits (rooms)

--- Dungeon/DungeonRoom.cs
+++ Dungeon/DungeonRoom.cs
@@ -3,10 +3,62 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RoomLibrary;
 
 namespace DungeonLibrary
 {
     internal class DungeonRoom
     {
-    }
-}
+        static void Main(string[] args)
+        {
+
+            {
+                bool mainLoop = true;
+                bool innerLoop = true;
+                Random rand = new Random();
+
+                Console.WriteLine("Dungeon Looping");
+                //TODO Create a player
+                Console.WriteLine("Welcome traveler, may I ask whom am I speaking to?\n");
+                Console.WriteLine("\nEnter Your Character Name\n"); ;
+
+                bool MainLoop = true;//boolean for outer loop. Exit game when true
+
+            }while (true)
+
+
+                do
+                {
+                    Rooms r1 = Rooms.GetRoom(); 
+                    Rooms r2 = Rooms.GetRoom(); 
+                    Rooms r3 = Rooms.GetRoom(); 
+
+
+
+
+                    Random rand = new Random();
+                    //TODO retrieve a random monster
+                    Console.WriteLine("You have stubbled upon (Insert Monster Here)\n");
+                    Console.WriteLine("");
+                    /*TODO generate a room
+                        * Implement GetRoom()
+                        */
+
+                    Console.WriteLine(r1);
+                    Console.WriteLine("");
+
+                    Console.WriteLine(r2);
+                    Console.WriteLine(""); 
+
+                    Console.WriteLine(r3);
+                    Console.WriteLine("");
+
+                    bool InnerLoop = true;
+
+                } while (true);//end do
+
+
+        }//end Main()
+
+    }//end DungeonRoom
+}//end namespace
---
+++ RoomLibrary/Rooms.cs
@@ -0,0 +1,55 @@
+﻿namespace RoomLibrary
+{
+    public class Rooms
+    {
+        private string _room;
+        //private string _description;
+
+
+        public string Room
+        {
+            get { return _room; }
+            set { _room = value; }
+        }
+        //public string description
+        //{
+        //    get { return _description; } 
+        //    set { _description = value; }
+        //}
+
+        public Rooms(string room, string description)
+        {
+            _room = room;
+            //_description = description;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Room: {Room}";
+             //$"Description: {description}";
+        }
+        public static Rooms GetRoom()
+        {
+            Rooms r1 = new Rooms("This is dark and stinky", "Layer");
+            return r1;
+            
+            Rooms r2 = new Rooms("This is hot and stinky", "Layer");
+            return r2;
+            
+            Rooms r3 = new Rooms("This is cold and stinky", "Layer");
+            return r3;
+        }
+        //public override string ToString()
+        //{
+        //    return $"Room: {Room}";
+        //     //$"Description: {description}";
+        //}
+        //public static Rooms GetRoom()
+        //{
+        //    Rooms r1 = new Rooms("This is dark and stinky", "Layer");
+        //    return r1;
+        //}
+        
+    }
+}
